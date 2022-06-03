41bd159c103d17442dfaf33c99a9fe449fe0fa1a
Sukhvindra9565 <78318189+Sukhvindra9565@users.noreply.github.com>
2022-06-03 17:26:13 +0530

    Create insert_a_node_in_afterAnode_in _singlyList.c

---
+++ insert_a_node_in_afterAnode_in _singlyList.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+int linkedListTraverse(struct Node *ptr)
+{
+  while(ptr!=NULL)
+  {
+    printf("Element = %d\n",ptr->data);
+    ptr = ptr->next;
+  }
+}
+struct Node* insertAfterNode(struct Node* head,
+struct Node *prevNode,int data)
+{
+  struct Node* ptr;
+  ptr=(struct Node*)malloc(sizeof(struct Node));
+ 
+  ptr->data = data;
+  ptr->next = prevNode->next;
+  prevNode->next = ptr;
+  return head;
+}
+int main()
+{
+  struct Node* head,*second,*third;
+  head = (struct Node*)malloc(sizeof(struct Node));
+  second = (struct Node*)malloc(sizeof(struct Node));
+  third = (struct Node*)malloc(sizeof(struct Node));
+  
+  // head linked with second
+  head->data = 12;
+  head->next = second;
+  // second linked with third
+  second->data = 24;
+  second->next = third;
+  // third is the end of list
+  third->data = 36;
+  third->next = NULL;
+  
+  head = insertAfterNode(head,third,48);
+  linkedListTraverse(head);
+  return 0;
+}
